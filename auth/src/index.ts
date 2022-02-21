c4942490c3cdc94d8798cb48f129c9a9c8d82736
Matt Mischewski <matt@dreamineering.com>
2022-02-21 18:10:21 +0100

    setup middleware error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHander } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
@@ -13,6 +14,8 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.use(errorHander);
 
 app.listen(3000, () => {
   console.log("listening on port 3000!!!!");
