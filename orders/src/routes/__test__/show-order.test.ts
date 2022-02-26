9fb869452e3c6593bfb5fcc0b263ff1ec6d18801
Matt Mischewski <matt@dreamineering.com>
2022-02-26 11:15:50 +0100

    get an order

---
+++ orders/src/routes/__test__/show-order.test.ts
@@ -0,0 +1,56 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { Ticket } from "../../models/ticket";
+
+it("fetches an order", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  ticket.save();
+
+  // make a request to build an order with this ticket
+  const user = global.getAuthCookie();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // act
+  // make a request to fetch the order
+  const { body: getOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .expect(200);
+
+  expect(getOrder.id).toEqual(order.id);
+});
+
+it("returns an error a user tries access another users order", async () => {
+  // create a ticket
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  ticket.save();
+
+  // make a request to build an order with this ticket
+  const user = global.getAuthCookie();
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // act and assert
+  // a different user cannot fetch the order
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set("Cookie", global.getAuthCookie())
+    .expect(400);
+});
--- orders/src/routes/show-order.ts
+++ orders/src/routes/show-order.ts
@@ -1,9 +1,28 @@
 import express, { Request, Response } from "express";
 
+import { requireAuth, NotFoundError } from "@stackmates/common";
+
+import { Order } from "../models/order";
+import { NotAuthorisedError } from "./../../../common/src/errors/not-authorised-error";
+
 const router = express.Router();
 
-router.get("/api/orders/:id", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError("Order not found");
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorisedError();
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
