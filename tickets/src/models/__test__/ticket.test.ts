ecab014cec73c6cf1e1d00152820693cb84eb854
Matt Mischewski <matt@dreamineering.com>
2022-02-28 09:12:30 +0100

    confirm db version on save works

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -42,3 +42,20 @@ it("implements optimistic concurrency", async () => {
 
   throw new Error("Should not reach this point");
 });
+
+it("increments the version number when saving", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    userId: "123",
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
