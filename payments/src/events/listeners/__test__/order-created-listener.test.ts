a838a256d50ca735cbe90bb0867a02f5effa19f8
Matt Mischewski <matt@dreamineering.com>
2022-03-01 08:45:04 +0100

    create payment orders listener

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,47 @@
+import { OrderCreatedEvent, OrderStatus } from "@stackmates/common";
+import mongoose from "mongoose";
+import { Order } from "../../../models/order";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCreatedListener } from "../order-created-listener";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: "asdf",
+    userId: "this",
+    status: OrderStatus.Created,
+    ticket: {
+      id: "jsdfj",
+      price: 10,
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg };
+};
+
+it("saves an order to process", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const order = await Order.findById(data.id);
+
+  expect(order).toBeDefined();
+  expect(order!.price).toEqual(data.ticket.price);
+});
+
+it("acks the message", async () => {
+  const { listener, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  expect(msg.ack).toHaveBeenCalled();
+});
---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@stackmates/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import { queueGroupName } from "./queue-group-name";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const order = await Order.build({
+      id: data.id,
+      version: data.version,
+      price: data.ticket.price,
+      userId: data.userId,
+      status: data.status,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
